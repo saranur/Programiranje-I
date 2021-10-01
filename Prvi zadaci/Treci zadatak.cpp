98526268728d7d1002e8824f81b65d8a1aa7e141
Sara <sara.nuredinovski@edu.fit.ba>
2021-10-01 23:19:08 +0200

    commit

---
+++ Prvi zadaci/Treci zadatak.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int noviBroj(int, int&);
+int main()
+{
+	int broj = 0, rezultat = 0;
+	do
+	{
+		cout << "Unesite cifru:  ";
+		cin >> broj;
+		if (broj > 9)
+			cout << "Cifra nije validna. " << endl;
+		if (broj > 0 && broj <= 9)
+			noviBroj(broj, rezultat);
+		
+
+	} while (broj > 0);
+	cout << "Novi broj je: "<< rezultat;
+
+	return 0;
+	system("pause>0");
+}
+int noviBroj(int broj, int& rezultat){
+	return rezultat = rezultat* 10 + broj;
+}
+
+
